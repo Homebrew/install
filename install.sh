16b5bc0a00d208b4402fccf254632871e7669fdf
itchyny <itchyny@hatena.ne.jp>
2020-01-23 20:50:20 +0900

    implement ohai and warn in bash

--- install.sh
+++ install.sh
@@ -30,23 +30,29 @@ tty_red="$(tty_mkbold 31)"
 tty_bold="$(tty_mkbold 39)"
 tty_reset="$(tty_escape 0)"
 
+shell_join() {
+  local arg
+  printf "%s" "$1"
+  shift
+  for arg in "$@"; do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+chomp()
+{
+  printf "%s" "${1/"$'\n'"/}"
+}
+
+ohai() {
+  printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+warn() {
+  printf "${tty_red}Warning${tty_reset}: %s\n" "$(chomp "$1")"
+}
+
 exit
-class Array
-  def shell_s
-    cp = dup
-    first = cp.shift
-    cp.map { |arg| arg.gsub " ", "\\ " }.unshift(first).join(" ")
-  end
-end
-
-def ohai(*args)
-  puts "#{Tty.blue}==>#{Tty.bold} #{args.shell_s}#{Tty.reset}"
-end
-
-def warn(warning)
-  puts "#{Tty.red}Warning#{Tty.reset}: #{warning.chomp}"
-end
-
 def system(*args)
   abort "Failed during: #{args.shell_s}" unless Kernel.system(*args)
 end
