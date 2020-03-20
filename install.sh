6f9b04355b11f6849fed761725a1f10cb326e979
Maxim Belkin <maxim.belkin@gmail.com>
2020-03-20 14:58:08 -0700

    install.sh: fix shellcheck SC2155 error

--- install.sh
+++ install.sh
@@ -115,7 +115,8 @@ execute_sudo() {
 }
 
 getc() {
-  local save_state=$(/bin/stty -g)
+  local save_state
+  save_state=$(/bin/stty -g)
   /bin/stty raw -echo
   IFS= read -r -n 1 -d '' "$@"
   /bin/stty "$save_state"
