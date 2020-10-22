1b45d6dad2a1a9db160027d8048634e74af955e8
zlrs <zhuys123@gmail.com>
2020-10-22 13:25:23 +0800

    change index to tsinghua

--- install.sh
+++ install.sh
@@ -32,7 +32,8 @@ else
   GROUP="$(id -gn)"
   TOUCH="/bin/touch"
 fi
-BREW_REPO="https://github.com/Homebrew/brew"
+# BREW_REPO="https://github.com/Homebrew/brew"
+BREW_REPO="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
 
 # TODO: bump version when new macOS is released
 MACOS_LATEST_SUPPORTED="10.15"
