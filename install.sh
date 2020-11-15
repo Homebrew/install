Homebrew/install

commit 8f22b74e637c9c5cc556050d0c98b6b83969017d

install: recommend Rosetta only on macOS

install.sh
@@ -318,7 +318,8 @@ EOABORT
 fi
 
 UNAME_MACHINE="$(uname -m)"
-if [[ "$UNAME_MACHINE" == "arm64" ]]; then
+
+if [[ -n "${HOMEBREW_ON_LINUX-}" ]] && [[ "$UNAME_MACHINE" == "arm64" ]]; then
   abort "$(cat <<EOABORT
 Homebrew is not (yet) supported on ARM processors!
 Rerun the Homebrew installer under Rosetta 2.
@@ -326,7 +327,9 @@ If you really know what you are doing and are prepared for a very broken experie
   ${tty_underline}https://docs.brew.sh/Installation${tty_reset}
 EOABORT
 )"
-elif [[ "$UNAME_MACHINE" != "x86_64" ]]; then
+fi
+
+if [[ "$UNAME_MACHINE" != "x86_64" ]]; then
   abort "Homebrew is only supported on Intel processors!"
 fi
 

commit 31ca11aad82c7e650fc6e10e6fd229643f7b8ac7

install: Return rosetta advice on arm64 macOS
Current version of installer prints a terse error on DTK:

  Homebrew is only supported on Intel processors!

Fixes: 8f22b74e637 ("install: recommend Rosetta only on macOS")

install.sh
@@ -319,7 +319,7 @@ fi
 
 UNAME_MACHINE="$(uname -m)"
 
-if [[ -n "${HOMEBREW_ON_LINUX-}" ]] && [[ "$UNAME_MACHINE" == "arm64" ]]; then
+if [[ -z "${HOMEBREW_ON_LINUX-}" ]] && [[ "$UNAME_MACHINE" == "arm64" ]]; then
   abort "$(cat <<EOABORT
 Homebrew is not (yet) supported on ARM processors!
 Rerun the Homebrew installer under Rosetta 2.
@@ -329,7 +329,7 @@ EOABORT
 )"
 fi
 
-if [[ "$UNAME_MACHINE" != "x86_64" ]]; then
+if [[ -n "${HOMEBREW_ON_LINUX-}" ]] && [[ "$UNAME_MACHINE" != "x86_64" ]]; then
   abort "Homebrew is only supported on Intel processors!"
 fi
 
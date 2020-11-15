31ca11aad82c7e650fc6e10e6fd229643f7b8ac7
Roman Bolshakov <r.bolshakov@yadro.com>
2020-11-15 06:12:53 +0300

    install: Return rosetta advice on arm64 macOS
    Current version of installer prints a terse error on DTK:
    
      Homebrew is only supported on Intel processors!
    
    Fixes: 8f22b74e637 ("install: recommend Rosetta only on macOS")

--- install.sh
+++ install.sh
@@ -319,7 +319,7 @@ fi
 
 UNAME_MACHINE="$(uname -m)"
 
-if [[ -n "${HOMEBREW_ON_LINUX-}" ]] && [[ "$UNAME_MACHINE" == "arm64" ]]; then
+if [[ -z "${HOMEBREW_ON_LINUX-}" ]] && [[ "$UNAME_MACHINE" == "arm64" ]]; then
   abort "$(cat <<EOABORT
 Homebrew is not (yet) supported on ARM processors!
 Rerun the Homebrew installer under Rosetta 2.
@@ -329,7 +329,7 @@ EOABORT
 )"
 fi
 
-if [[ "$UNAME_MACHINE" != "x86_64" ]]; then
+if [[ -n "${HOMEBREW_ON_LINUX-}" ]] && [[ "$UNAME_MACHINE" != "x86_64" ]]; then
   abort "Homebrew is only supported on Intel processors!"
 fi
 
