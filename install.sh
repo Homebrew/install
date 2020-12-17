Homebrew/install

commit 6f37ca94af073c2971efbed8aa293322aa171f26

install.sh: use NONINTERACTIVE in more places.

install.sh
@@ -298,7 +298,10 @@ fi
 if [[ -z "${HOMEBREW_ON_LINUX-}" ]]; then
  have_sudo_access
 else
-  if [[ -n "${CI-}" ]] || [[ -w "$HOMEBREW_PREFIX_DEFAULT" ]] || [[ -w "/home/linuxbrew" ]] || [[ -w "/home" ]]; then
+  if [[ -n "${NONINTERACTIVE-}" ]] ||
+     [[ -w "$HOMEBREW_PREFIX_DEFAULT" ]] ||
+     [[ -w "/home/linuxbrew" ]] ||
+     [[ -w "/home" ]]; then
     HOMEBREW_PREFIX="$HOMEBREW_PREFIX_DEFAULT"
   else
     trap exit SIGINT
@@ -479,7 +482,7 @@ if should_install_command_line_tools; then
   ohai "The Xcode Command Line Tools will be installed."
 fi
 
-if [[ -t 0 && -z "${CI-}" ]]; then
+if [[ -z "${NONINTERACTIVE-}" ]]; then
   wait_for_user
 fi
 

commit 1ccc34a60c4dd345f0f30ed496f434450f3b058b

Fix: using wrong HOMEBREW_PREFIX
The comment in the file https://github.com/Homebrew/install/blob/master/install.sh#L17 specifically mention that it is possible to run linuxbrew in `~/.linuxbrew` 
However, it looks like https://github.com/Homebrew/install/commit/6f37ca94af073c2971efbed8aa293322aa171f26 broke this behaviour. 

I believe this is because, once you run it NONITERACTIVE, the IF statement is True, causing it to branch into the wrong path.
This fixes that.

Also relates to https://github.com/Homebrew/install/issues/369

install.sh
@@ -298,9 +298,9 @@ fi
 if [[ -z "${HOMEBREW_ON_LINUX-}" ]]; then
  have_sudo_access
 else
-  if [[ -n "${NONINTERACTIVE-}" ]] ||
-     [[ -w "$HOMEBREW_PREFIX_DEFAULT" ]] ||
-     [[ -w "/home/linuxbrew" ]] ||
+  if [[ -n "${NONINTERACTIVE-}" ]] &&
+     [[ -w "$HOMEBREW_PREFIX_DEFAULT" ]] &&
+     [[ -w "/home/linuxbrew" ]] &&
      [[ -w "/home" ]]; then
     HOMEBREW_PREFIX="$HOMEBREW_PREFIX_DEFAULT"
   else