01f87ad22391226b697e08845f4eb4441fa486d4
XuehaiPan <XuehaiPan@pku.edu.cn>
2021-09-01 16:10:14 +0800

    install.sh: use relative path for symlinks

--- install.sh
+++ install.sh
@@ -742,7 +742,11 @@ ohai "Downloading and installing Homebrew..."
   execute "git" "reset" "--hard" "origin/master"
 
   if [[ "${HOMEBREW_REPOSITORY}" != "${HOMEBREW_PREFIX}" ]]; then
-    execute "ln" "-sf" "${HOMEBREW_REPOSITORY}/bin/brew" "${HOMEBREW_PREFIX}/bin/brew"
+    if [[ "${HOMEBREW_REPOSITORY}" == "${HOMEBREW_PREFIX}/Homebrew" ]]; then
+      execute "ln" "-sf" "../Homebrew/bin/brew" "${HOMEBREW_PREFIX}/bin/brew"
+    else  # absolute path as fallback option (we probably never reach here)
+      execute "ln" "-sf" "${HOMEBREW_REPOSITORY}/bin/brew" "${HOMEBREW_PREFIX}/bin/brew"
+    fi
   fi
 
   if [[ ! -d "${HOMEBREW_CORE}" ]]; then
