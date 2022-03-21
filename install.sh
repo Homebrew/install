f4cd0498d9899828a340324c24dbfd0b621623aa
Mike McQuaid <mike@mikemcquaid.com>
2022-03-21 09:41:03 +0000

    install.sh: ask return or enter.
    Fixes https://github.com/Homebrew/install/issues/625

--- install.sh
+++ install.sh
@@ -256,7 +256,7 @@ ring_bell() {
 wait_for_user() {
   local c
   echo
-  echo "Press ${tty_bold}RETURN${tty_reset} to continue or any other key to abort:"
+  echo "Press ${tty_bold}RETURN${tty_reset}/${tty_bold}ENTER${tty_reset} to continue or any other key to abort:"
   getc c
   # we test for \r and \n because some stuff does \r instead
   if ! [[ "${c}" == $'\r' || "${c}" == $'\n' ]]
