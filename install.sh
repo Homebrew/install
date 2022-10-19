e84453419eacb7e20937d71c1f087a1573f398fe
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-10-19 11:27:21 +0200

    install.sh: support macOS Ventura

--- install.sh
+++ install.sh
@@ -179,9 +179,9 @@ fi
 export HOMEBREW_{BREW,CORE}_GIT_REMOTE
 
 # TODO: bump version when new macOS is released or announced
-MACOS_NEWEST_UNSUPPORTED="13.0"
+MACOS_NEWEST_UNSUPPORTED="14.0"
 # TODO: bump version when new macOS is released
-MACOS_OLDEST_SUPPORTED="10.15"
+MACOS_OLDEST_SUPPORTED="11.0"
 
 # For Homebrew on Linux
 REQUIRED_RUBY_VERSION=2.6    # https://github.com/Homebrew/brew/pull/6556
