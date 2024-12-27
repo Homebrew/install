80c41b0e07845a43b1ba156469d475dadc1aba74
Jobs Gate <organicchemistry_01@yahoo.com.ph>
2024-12-27 20:39:26 +0800

    Fix CL formatting error

--- install.sh
+++ install.sh
@@ -174,11 +174,11 @@ else
 
   HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
 
-  if [ -z "$HOMEBREW_PREFIX" ]; then
-      echo "HOMEBREW_PREFIX is not set. Installing to $HOMEBREW_PREFIX"
+  if [[ -z "$HOMEBREW_PREFIX" ]]; then
+      echo "HOMEBREW_PREFIX is not set. Installing to ${HOMEBREW_PREFIX}"
   else
-      echo "HOMEBREW_PREFIX is set to: $HOMEBREW_PREFIX"
-      echo "Installing to $HOMEBREW_PREFIX"
+      echo "HOMEBREW_PREFIX is set to: ${HOMEBREW_PREFIX}"
+      echo "Installing to ${HOMEBREW_PREFIX}"
   fi
   
   HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}/Homebrew"
